c9adf3ba2a0caa2b6b9a7cf3876d5fe68afc9c0b
sastra <sastrawiguna596@gmail.com>
2026-09-09 14:27:06 +0800

    baru buat fitur nilai

--- app/Models/Assessment.php
+++ app/Models/Assessment.php
@@ -11,4 +11,30 @@ class Assessment extends Model
     use HasFactory, HasUuids;
 
     protected $guarded = [];
+
+    protected $casts = [
+        'score'           => 'decimal:2',
+        'weight'          => 'decimal:2',
+        'assessment_date' => 'date',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function enrollment()
+    {
+        return $this->belongsTo(Enrollment::class);
+    }
+
+    public function programLevel()
+    {
+        return $this->belongsTo(ProgramLevel::class);
+    }
+
+    public function recordedBy()
+    {
+        return $this->belongsTo(User::class, 'recorded_by');
+    }
 }
